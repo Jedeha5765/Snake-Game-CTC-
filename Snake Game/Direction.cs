952b0d8c61a4c4373c944392ebd5e4fbc9f1c042
kids <kids@MEH>
2024-01-05 21:21:30 -0500

    Add project files.

---
+++ Snake Game/Direction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+
+        public int Rowoffset { get; }
+        public int Coloffset { get; }
+
+        private Direction(int rowOffset, int colOffset)
+        {
+            Rowoffset = rowOffset;
+            Coloffset = colOffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-Rowoffset, -Coloffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   Rowoffset == direction.Rowoffset &&
+                   Coloffset == direction.Coloffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Rowoffset, Coloffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+    }
+}
